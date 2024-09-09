2cfd57cb58efceebf2b194e5283ec9b6e2194ae2
Italo Silva <italo@maleldil.com>
2024-09-09 18:23:12 +0100

    random: Add script to auto-generate list of contents from docstrings
    There's no way I'll maintain a README list with all this random stuff,
    but I try to always add a docstring to everything, so why not use that?
    
    I could further automate this to add the content to the README, but that
    sounds too hard.

--- random/README.md
+++ random/README.md
@@ -1,3 +1,17 @@
 # random
 
 Weird and random code fragments that might or might not be helpful some day.
+
+## Files
+
+- [`countsymbols.go`](countsymbols.go): Count ASCII symbols in code files in a given
+  directory.
+- [`markdown_word_counter.go`](markdown_word_counter.go): Excludes headings, code blocks
+  and indented blocks.
+- [`find_forelse.py`](find_forelse.py): Count occurences of for-else in Python files.
+- [`docstrings.py`](docstrings.py): Generate a list of files and descriptions from
+  Python and Go module docstrings.
+- [`argenum.py`](argenum.py): Use an Enum for argparse choices.
+- [`gzjson.py`](gzjson.py): Demonstrate reading a JSON file that may be gzipped.
+- [`text_metrics.py`](text_metrics.py): Calculate similarity metrics (Exact Match, F1
+  score, and ROUGE-L) between two strings.
---
+++ random/docstrings.py
@@ -0,0 +1,81 @@
+"""Generate a list of files and descriptions from Python and Go module docstrings."""
+
+import argparse
+import ast
+import re
+import textwrap
+
+from pathlib import Path
+from typing import TextIO
+
+
+def extract_py_docstring(file_path: Path) -> str | None:
+    module = ast.parse(file_path.read_text())
+
+    if docstring := ast.get_docstring(module):
+        return docstring.splitlines()[0].strip()
+    return None
+
+
+def extract_go_docstring(file_path: Path) -> str | None:
+    content = file_path.read_text()
+
+    if match := re.search(
+        r"((?:\/\/.*?\n|\/\*(?:.|[\r\n])*?\*\/)\s*)+package\s+\w+",
+        content,
+        re.DOTALL,
+    ):
+        comments = match[1].strip()
+        # Remove comment symbols and extra whitespace
+        docstring = re.sub(r"(^|\n)\s*(//)|\s*(/\*|\*/)", "\n", comments).strip()
+        return docstring.splitlines()[0].strip()
+
+    return None
+
+
+EXTRACT_FN = {".py": extract_py_docstring, ".go": extract_go_docstring}
+
+
+def main(input_path: Path, output_file: TextIO) -> None:
+    docstrings: dict[Path, str] = {}
+
+    for file_path in input_path.rglob("*"):
+        if file_path.suffix not in EXTRACT_FN:
+            continue
+        if docstring := EXTRACT_FN[file_path.suffix](file_path):
+            docstrings[file_path] = docstring
+
+    output_file.write("## Files\n\n")
+    for file_name, docstring in docstrings.items():
+        initial_line = f"- [`{file_name}`]({file_name}): "
+        remaining_width = 88 - len(initial_line)
+
+        wrapped_docstring = textwrap.fill(docstring, width=remaining_width)
+        wrapped_lines = wrapped_docstring.split("\n")
+
+        output_file.write(f"{initial_line}{wrapped_lines[0]}\n")
+
+        for line in wrapped_lines[1:]:
+            output_file.write(f"  {line}\n")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description=__doc__, formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+    parser.add_argument(
+        "input_path",
+        nargs="?",
+        default=".",
+        type=Path,
+        help="The path to the directory containing the source files",
+    )
+    parser.add_argument(
+        "output_file",
+        nargs="?",
+        default="-",
+        type=argparse.FileType("w"),
+        help="The path to the output file where the docstrings will be written",
+    )
+    args = parser.parse_args()
+    main(args.input_path, args.output_file)
