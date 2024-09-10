f1dc7fd4c3eaeb6876f31e2a670cdbadd1fd440c
Italo Silva <italo@maleldil.com>
2024-09-10 14:21:54 +0100

    s2orc: Rewrite the venues script, as the old was too slow
    I don't know why this is faster, but I'll let it be.

--- s2orc/unique_venues.fish
+++ s2orc/unique_venues.fish
@@ -1,6 +1,5 @@
 #!/usr/bin/env fish
 # Gets the venue of each paper in the gzipped data files and prints the unique ones.
-# Files come from the ./download_s2orc.py script
-for f in $argv
-    gzip -dc $f | jq -r 'map(.venue | ascii_downcase)[]'
-end | sort -u
+# Files come from the ./download_s2orc.py script, saved in the `data` directory.
+fd -HI -ejson.gz . data -x sh -c 'gzip -dc {} |  jq -r \'map(.venue)[]\'' |
+    string trim | string lower | sort -u
