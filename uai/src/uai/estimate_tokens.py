82a337b16a81b261b803c9a0a536e665012f27d6
Italo Silva <italo@maleldil.com>
2025-05-09 12:09:25 +0100

    uai(ntok): Use helper function to read from file or -

--- uai/src/uai/estimate_tokens.py
+++ uai/src/uai/estimate_tokens.py
@@ -1,26 +1,24 @@
 """Estimate the number of tokens in the input file (or stdin) using the GPT-4 tokeniser."""
 
-import sys
 from pathlib import Path
 from typing import Annotated
 
 import tiktoken
 import typer
 
+from uai.util import read_text
+
 # GPT-4 and later tokeniser.
 _GPT_TOKENISER = tiktoken.get_encoding("o200k_base")
 
 
 def main(
-    file: Annotated[
-        Path | None,
+    path: Annotated[
+        Path,
         typer.Argument(help="Path to the file. If not provided, use stdin."),
-    ] = None,
+    ] = Path("-"),
 ):
-    if file is None:
-        text = sys.stdin.read()
-    else:
-        text = file.read_text()
+    text = read_text(path)
 
     try:
         num_tokens = len(_GPT_TOKENISER.encode(text))
